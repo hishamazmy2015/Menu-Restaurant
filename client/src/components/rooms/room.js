af07f8c6ba14d71429e7790873a32d49d92e9639
Hesham Azmy <viji@tpconnects.com>
2021-07-01 12:16:49 +0400

    room styles

--- client/src/components/rooms/room.js
+++ client/src/components/rooms/room.js
@@ -12,96 +12,119 @@ const Room = ({ getCurrentRoomData, auth, room }) => {
   return (
     <div>
       {room && room !== null && room.menu !== null && room.menu.length > 0 && (
-        <li>
-          {/* <img src={room.info.image_url} /> */}
+        <div style={{ margin: '3%', position: 'relative' }}>
+          <img src={room.info.image_url} />
+          <span class='d-block'>
+            <h1
+              style={{
+                // margin: '3%',
+                // position: 'relative',
+                position: 'absolute',
+                top: '470px',
+                left: '20px',
+                color: 'white',
+              }}
+            >
+              In Room Dining
+            </h1>
+          </span>
           {/* <img src={room.info.logo_url} /> */}
-        </li>
+        </div>
       )}
-      <Tabs>
-        {room &&
-          room !== null &&
-          room.menu !== null &&
-          room.menu.length > 0 &&
-          room.menu.map((room) => (
+      {room &&
+        room !== null &&
+        room.menu !== null &&
+        room.menu.length > 0 &&
+        room.menu.map((room) => (
+          <div>
+            <h1
+              style={{
+                position: 'absolute',
+                right: '50%',
+                margin: 'auto',
+              }}
+            >
+              {room.menu_name}
+            </h1>
+
             <div>
-              <h1
-                style={{
-                  position: 'absolute',
-                  right: '50%',
-                  margin: 'auto',
-                }}
-              >
-                {room.menu_name}
-              </h1>
+              {room.menu_sections &&
+                room.menu_sections.map((sec) => (
+                  <div>
+                    {/* {sec.section_name.toString()} */}
+                    key={sec.id}
+                    value='banana' header={sec.section_name.toString()}
+                    {sec.menu_categories.map((cat) => (
+                      <div className='card-body'>
+                        <Tabs>
+                          <Tab value={cat.name} header={cat.name}>
+                            {cat.name}
+                            <ul>
+                              {cat.items.map((item) => (
+                                <div class='d-flex justify-content-between align-items-center mt-3 p-2 items rounded'>
+                                  <div class='d-flex flex-row align-items-center'>
+                                    <li>
+                                      <ul
+                                        class='columns'
+                                        data-columns='2'
+                                        style={{
+                                          display: 'grid',
+                                          'grid-template-columns':
+                                            '1fr 3fr 1fr  ',
+                                          'grid-template-rows':
+                                            '50px auto 70px  ',
+                                        }}
+                                      >
+                                        {/* <li> */}
+                                        {item.image ? (
+                                          <img
+                                            style={{
+                                              width: 100,
+                                              height: 100,
+                                            }}
+                                            src={item.image}
+                                          />
+                                        ) : (
+                                          <img
+                                            style={{
+                                              width: 100,
+                                              height: 100,
+                                            }}
+                                            src='https://dxp-hospitality-dev-rg-sit-467300-cd2.azurewebsites.net//-/mediadh/dh/hospitality/e-menu/menus/alnpool/96178.jpg'
+                                          />
+                                        )}
 
-              <div>
-                {room.menu_sections &&
-                  room.menu_sections.map((sec) => (
-                    <div>
-                      {/* {sec.section_name.toString()} */}
-                      key={sec.id}
-                      value='banana' header={sec.section_name.toString()}
-                      {sec.menu_categories.map((cat) => (
-                        <Tab value={cat.name} header={cat.name}>
-                          {cat.name}
-                          <ul>
-                            {cat.items.map((item) => (
-                              <div class='d-flex justify-content-between align-items-center mt-3 p-2 items rounded'>
-                                <div class='d-flex flex-row align-items-center'>
-                                  {/* <ul class='columns' data-columns='2'> */}
-
-                                  <li>
-                                    <ul
-                                      class='columns'
-                                      data-columns='2'
-                                      style={{
-                                        display: 'grid',
-                                        'grid-template-columns':
-                                          '1fr 3fr 1fr  ',
-                                        'grid-template-rows':
-                                          '50px auto 70px  ',
-                                      }}
-                                    >
-                                      {/* <li> */}
-                                      {item.image ? (
-                                        <img
-                                          style={{
-                                            width: 100,
-                                            height: 100,
-                                          }}
-                                          src={item.image}
-                                        />
-                                      ) : (
-                                        <img
-                                          style={{
-                                            width: 100,
-                                            height: 100,
-                                          }}
-                                          src='https://dxp-hospitality-dev-rg-sit-467300-cd2.azurewebsites.net//-/mediadh/dh/hospitality/e-menu/menus/alnpool/96178.jpg'
-                                        />
-                                      )}
-
-                                      <span class='d-block'>
-                                        <li>{item.name}</li>
-                                        <li>{item.price}</li>
-                                      </span>
-                                    </ul>
-                                  </li>
+                                        <span class='d-block'>
+                                          <li>{item.name}</li>
+                                          <li>{item.price}</li>
+                                        </span>
+                                      </ul>
+                                    </li>
+                                  </div>
                                 </div>
-                              </div>
-                            ))}
-                          </ul>
-                        </Tab>
-                      ))}
-                    </div>
-                  ))}
-              </div>
+                              ))}
+                            </ul>
+                          </Tab>
+
+                          
+                        </Tabs>
+                      </div>
+                    ))}
+                  </div>
+                ))}
             </div>
-          ))}
-        <Tab value='apple50' header='All Days 50'>
-          All Days sss
-        </Tab>
-      </Tabs>
+          </div>
+        ))}
+      {/* {
+          <Tab value='apple50' header='All Days 50'>
+            All Days sss
+          </Tab>
+        }
+        {
+          <Tab value='apple50' header='All Days 50'>
+            All Days sss
+          </Tab>
+        } */}
     </div>
   );
 };
